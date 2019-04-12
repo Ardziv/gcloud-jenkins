d86c2760d325d86bb4d634c5c6b6c6d7f597ae2d
Frederic Alex <frederic.alex@cloudworx.asia>
2019-04-12 15:57:22 +0800

    init

--- generateJenkins.sh
+++ generateJenkins.sh
@@ -8,7 +8,7 @@ cp *.groovy $tmpdir
 echo "Building server on Google Cloud, this may  take a few moments..."
 ./create_build_server.sh
 echo reticulating splines....
-gcloud compute copy-files $tmpdir/*.groovy  $instancename:~/.  --zone $zone
+gcloud compute scp $tmpdir/*.groovy  $instancename:~/.  --zone $zone
 echo copied groovy scripts over to remote server
 echo waiting for server instance to come up
 # Give Jenkins a minute to start up. I'll replace this with a loop at some point, or maybe leave that as an exercise for the reader!
@@ -23,7 +23,7 @@ echo "installing plugins (this will restart Jenkins)"
 gcloud compute ssh $instancename --zone $zone --command "curl --data-urlencode 'script@./configurePlugins.groovy' -u${managerUser}:${managerPassword} http://localhost:8080/scriptText"
 
 #Copy public key back over
-gcloud compute copy-files $instancename:~/id_rsa.pub ../id_rsa.pub  --zone $zone
+gcloud compute scp $instancename:~/id_rsa.pub ../id_rsa.pub  --zone $zone
 
 echo Executed script to connect Jenkins to LDAP
 echo ===========================================
@@ -31,6 +31,6 @@ echo
 echo
 echo 
 rm -Rf $tmpdir
-echo "Ok, that's all done, I've just tidied up around here. Have a nice day :) - BestBoyElectric.io"
+echo "Ok, that's all done."
 gcloud compute instances describe --zone $zone  $instancename | grep natIP | awk '{print "Your new Jenkins server is running at http://" $2 ":8080"}'
 echo "To connect your Jenkins instance to your Github, add the key in id_rsa.pub to your GitHub project"
