cc1e2e0923e5c685a23098687bced4ed8db392d4
Frederic Alex <frederic.alex@cloudworx.asia>
2019-04-12 16:02:12 +0800

    init

--- client_scripts/create_build_server.sh
+++ client_scripts/create_build_server.sh
@@ -1,5 +1,5 @@
 . ../settings.conf
-gcloud compute instances create $instancename --metadata-from-file startup-script=./install_jenkins.sh --image "ubuntu-15-10" --machine-type "n1-standard-1" --zone "$zone" 
+gcloud compute instances create $instancename --metadata-from-file startup-script=./install_jenkins.sh --image "$image" --machine-type "$machine_type" --zone "$zone" 
 
 gcloud compute firewall-rules create jenkins-web-port --allow tcp:8080
 
